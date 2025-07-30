58345c231239af73006f53a6fe95864b7a8d13d1
ancadordea <ancaedordea@gmail.com>
2025-07-30 15:08:09 +0100

    idempotent against python interpreter working dir

--- 1.raw_data/processing_to_csv.py
+++ 1.raw_data/processing_to_csv.py
@@ -3,12 +3,12 @@ import pandas as pd
 import os
 
 # Globals
-input_excel = 'raw_sales_data.xlsx'
-output_folder = "2.processed_csv_data"
-
 this_file = os.path.abspath(__file__)
 script_dir = os.path.dirname(this_file)
 parent_dir = os.path.dirname(script_dir)
+
+input_excel = os.path.join(script_dir, 'raw_sales_data.xlsx')
+output_folder = "2.processed_csv_data"
 full_output_dir = os.path.join(parent_dir, output_folder)
 
 os.makedirs(full_output_dir, exist_ok=True)
